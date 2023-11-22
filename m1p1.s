b7981393d60a1fe35227a07fea615297a887f863
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-11-22 20:18:49 +0200

    wwww

--- m1p1.s
+++ m1p1.s
@@ -2,6 +2,7 @@
 m1p1:
     push {r4-r7, lr}  // Save registers
     mov r4, r0  // Move the pointer to r4 for manipulation
+    mov r6, #1  // Initialize a flag to check if the current character is the first character of a word
 
 convert_loop:
     ldrb r5, [r4]  // Load the byte at the pointer into r5
@@ -12,17 +13,32 @@ convert_loop:
     cmp r5, #32
     beq next_char
 
-    // Check if the character is lowercase (ASCII 97 to 122)
-    cmp r5, #97
+    // Check if the character is uppercase (ASCII 65 to 90)
+    cmp r5, #65
     blt next_char
-    cmp r5, #122
+    cmp r5, #90
     bgt next_char
 
-    // Convert to uppercase by subtracting 32
-    sub r5, r5, #32
+    // Check if the current character is the first character of a word
+    cmp r6, #1
+    bne next_char
+
+    // Convert to lowercase by adding 32
+    add r5, r5, #32
     strb r5, [r4]
 
 next_char:
+    // Check if the character is a space (ASCII 32)
+    cmp r5, #32
+    bne not_space
+
+    // If it's a space, set the flag to 1
+    mov r6, #1
+    b convert_loop
+
+not_space:
+    // If it's not a space, set the flag to 0
+    mov r6, #0
     add r4, r4, #1  // Move to the next character
     b convert_loop  // Go back to the start of the loop
 
