b8949034d3164d30d66f8c951f690edfd1e517db
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-11-22 20:16:31 +0200

    qqew

--- m1p1.s
+++ m1p1.s
@@ -1,32 +1,30 @@
 .global m1p1
 m1p1:
-    push {lr}              @ save the return address
-    mov r1, #0             @ initialize a flag to check if the previous character was a space
+    push {r4-r7, lr}  // Save registers
+    mov r4, r0  // Move the pointer to r4 for manipulation
 
-convert:
-    ldrb r2, [r0], #1      @ load the current character into r2 and increment the pointer
-    cmp r2, #0             @ compare the current character with null
-    beq end                @ if it's null, we've reached the end of the string, so branch to end
+convert_loop:
+    ldrb r5, [r4]  // Load the byte at the pointer into r5
+    cmp r5, #0  // Compare the byte to 0 (end of string)
+    beq end_convert  // If it's 0, we're done, so branch to end
 
-    cmp r2, #' '           @ compare the current character with space
-    moveq r1, #1           @ if it's a space, set the flag to 1
-    bne next               @ if it's not a space, branch to next
+    // Check if the character is a space (ASCII 32)
+    cmp r5, #32
+    beq next_char
 
-next:
-    cmp r1, #1             @ check if the previous character was a space
-    bne lower              @ if it wasn't, branch to lower
-    sub r2, r2, #'a'-'A'   @ if it was, convert the current character to uppercase
-    strb r2, [r0, #-1]     @ store the converted character back into the string
-    mov r1, #0             @ reset the flag
-    b convert              @ branch back to convert
+    // Check if the character is lowercase (ASCII 97 to 122)
+    cmp r5, #97
+    blt next_char
+    cmp r5, #122
+    bgt next_char
 
-lower:
-    cmp r2, #'a'
-    blt convert            @ if the character is not a lowercase letter, branch back to convert
-    cmp r2, #'z'
-    bgt convert            @ if the character is not a lowercase letter, branch back to convert
-    strb r2, [r0, #-1]     @ store the lowercase character back into the string
-    b convert              @ branch back to convert
+    // Convert to uppercase by subtracting 32
+    sub r5, r5, #32
+    strb r5, [r4]
 
-end:
-    pop {pc}               @ return to the caller
+next_char:
+    add r4, r4, #1  // Move to the next character
+    b convert_loop  // Go back to the start of the loop
+
+end_convert:
+    pop {r4-r7, pc}  // Restore registers and return
