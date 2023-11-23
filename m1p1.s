30b30a7249300a99b69c764c8855b294deba2e51
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-11-23 18:56:32 +0200

    goback

--- m1p1.s
+++ m1p1.s
@@ -1,50 +1,45 @@
 .global m1p1
-
 m1p1:
-    mov r6, #4  // Initialize counter
+    push {r4-r7, lr}  // Save registers
     mov r4, r0  // Copy pointer to r4
-    mov r5, #0  // Initialize counter for characters
+    mov r6, #1  // Initialize flag for first character of a word
 
-loop:
-    ldrb r3, [r4, r5]  // Load byte at pointer
-    cmp r3, #0  // Check for end of string
+convert_loop:
+    ldrb r5, [r4]  // Load byte at pointer
+    cmp r5, #0  // Check for end of string
     beq end_convert  // If end of string, exit loop
 
-    cmp r3, #32  // Check for space
-    beq capitalizeNext  // If space, skip to next character
+    cmp r5, #32  // Check for space
+    moveq r6, #1  // If space, set flag to uppercase next character
+    beq next_char
 
-    cmp r3, #'a'  // Check if character is lowercase
+    cmp r5, #97  // Check if character is lowercase
     blt skip_charater
-    cmp r3, #'z'
+    moveq r6, #0
+    cmp r5, #122
     bgt skip_charater
+    moveq r6, #0
 
-    cmp r6, #0  // If flag is set, convert to uppercase
-    subeq r3, r3, #32  // Convert to uppercase
+    cmp r6, #1  // If flag is set, convert to uppercase
+    subeq r5, r5, #32  // Convert to uppercase
     moveq r6, #0  // Reset flag
-    strb r3, [r4, r5]  // Store converted character
+    strb r5, [r4]  // Store converted character
     beq next_char
 
 skip_charater:
-    cmp r3, #'A'  // Check if character is uppercase
+    cmp r5, #65  // Check if character is uppercase
     blt next_char
-    cmp r3, #'Z'
+    cmp r5, #90
     bgt next_char
 
-    add r3, r3, #32  // Convert to lowercase
-    strb r3, [r4, r5]  // Store converted character
+    cmp r6, #0  // If flag is set, convert to lowercase
+    add r5, r5, #32  // Convert to lowercase
+    strb r5, [r4]  // Store converted character
     b next_char
 
-capitalizeNext:
-    add r5, r5, #1  // Move to next character
-    ldrb r3, [r4, r5]
-    sub r3, r3, #32  // Convert lowercase to uppercase
-    strb r3, [r4, r5]  // Store converted character
-    b loop
-
 next_char:
-    add r5, r5, #1  // Move to next character
-    cmp r5, r6  // Check if counter reached end
-    ble loop  // Repeat loop if not
+    add r4, r4, #1  // Move to next character
+    b convert_loop  // Repeat loop
 
 end_convert:
-    bx lr  // Return
+    pop {r4-r7, pc}  // Restore registers and return
