5650f9f7179a97747921f79766eaa3b42d3be682
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-11-23 18:40:48 +0200

    BRABABBA

--- m1p1.s
+++ m1p1.s
@@ -1,54 +1,52 @@
 .global m1p1
-
 m1p1:
-    mov r6, #4
-    
-    mov r4, r0
-    mov r5, #0 // counter
+    push {r4-r7, lr}  // Save registers
+    mov r4, r0  // Copy pointer to r4
+    mov r5, #0  // Initialize counter
+    mov r6, #1  // Initialize flag for first character of a word
 
-loop:
-    ldrb r3, [r4, r5] @ load the character from the string
+convert_loop:
+    ldrb r3, [r4, r5]  // Load byte at pointer
+    cmp r3, #0  // Check for end of string
+    beq end_convert  // If end of string, exit loop
 
-    cmp r3, #0
-    beq exit_loop
-
-    cmp r5, #0 @ check if it's the first character
+    cmp r5, #0  // Check if it's the first character
     beq capitalize
 
-    cmp r3, #' ' @ check if it's a space
+    cmp r3, #32  // Check for space
     beq capitalizeNext
 
-    cmp r3, #'Z' @ check if the character is equal to or greater than lowercase 'a'
-    ble lowercase @ if it's not, then skip
+    cmp r3, #90  // Check if the character is equal to or less than uppercase 'Z'
+    bgt lowercase  // If it's not, then skip to lowercase
 
-    bge skip
-
-skip:
-    add r5, r5, #1 @ inc the counter
-    b loop 
+    b skip
 
 capitalizeNext:
     add r5, r5, #1
     ldrb r3, [r4, r5]
-    sub r3, r3, #32  @ convert lowercase to uppercase
-    strb r3, [r0, r5]
+    sub r3, r3, #32  // Convert lowercase to uppercase
+    strb r3, [r4, r5]
     add r5, r5, #1
-    b loop
+    b convert_loop
 
 capitalize:
-    sub r3, r3, #32  @ convert lowercase to uppercase
-    strb r3, [r0, r5]
+    sub r3, r3, #32  // Convert lowercase to uppercase
+    strb r3, [r4, r5]
     add r5, r5, #1
-    b loop
+    b convert_loop
 
 lowercase:
-    cmp r3, #'A'
+    cmp r3, #65  // Check if the character is equal to or greater than uppercase 'A'
     blt skip
 
-    add r3, r3, #32  @ convert uppercase to lowercase
-    strb r3, [r0, r5]
+    add r3, r3, #32  // Convert uppercase to lowercase
+    strb r3, [r4, r5]
     add r5, r5, #1
-    b loop
+    b convert_loop
 
-exit_loop:
-    bx lr  
+skip:
+    add r5, r5, #1  // Increment the counter
+    b convert_loop  // Repeat loop
+
+end_convert:
+    pop {r4-r7, pc}  // Restore registers and return
