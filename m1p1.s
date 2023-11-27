b3404fcdc10a8ce7c530ba5effc6fd42022ff065
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-11-27 23:11:20 +0200

    Test

--- m1p1.s
+++ m1p1.s
@@ -9,44 +9,39 @@ convert_loop:
     cmp r5, #0  // Check for end of string
     beq end_convert  // If end of string, exit loop
 
-    cmp r5, #32  // Check for space
-    moveq r6, #1  // If space, set flag to uppercase next character
-    beq next_char
+    cmp r5, #' '
+    moveq r6, #1  // Set flag if character is a space
+    beq next_char  // Skip to next character if space
 
-    cmp r5, #97  // Check if character is lowercase
-    blt skip_charater
-    moveq r6, #0
-    cmp r5, #122
-    bgt next_char
-    moveq r6, #0
+    cmp r5, #122  // Check if character is uppercase
+    bgt next_char  // Skip to next character if uppercase
 
-    cmp r6, #1  // If flag is set, convert to uppercase
-    beq checkifuppercase
+    cmp r5, #65  // Check if character is lowercase
+    blt next_char  // Skip to next character if not lowercase
 
-skip_charater:
-    cmp r5, #65  // Check if character is uppercase
-    blt next_char
-    cmp r5, #90
-    bgt next_char
+    cmp r5, #'a'  // Check if character is lowercase
+    bge checkifneedswork  // Skip to next character if not lowercase
 
-    cmp r6, #0  // If flag is set, convert to lowercase
+    cmp r5, #'Z'  // Check if character is lowercase
+    bge lowercase
+
+
+checkifneedswork:
+    cmp r6, #1  // Check if first character of a word
+    beq capitalize  // If first character, capitalize
+
+    bne next_char  // If not first character, skip to next character
+
+lowercase:
     add r5, r5, #32  // Convert to lowercase
-    strb r5, [r4]  // Store converted character
-    b next_char
-
-checkifuppercase:
-    cmp r5, #90  // Check if character is uppercase
-    moveq r6, #0  // Reset flag
-    blt next_char
-
-    cmp r5, #97  // Check if character is lowercase
-    bgt capitalize
+    strb r5, [r4]  // Store lowercase character
+    b next_char  // Skip to next character
 
 capitalize:
     sub r5, r5, #32  // Convert to uppercase
-    strb r5, [r4]  // Store converted character
-    moveq r6, #0  // Reset flag
-    b next_char
+    strb r5, [r4]  // Store uppercase character
+    mov r6, #0  // Clear flag
+    b next_char  // Skip to next character
 
 next_char:
     add r4, r4, #1  // Move to next character
